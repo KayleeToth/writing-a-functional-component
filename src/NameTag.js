0c541a55aa5fd5a932319146df84f0b9099e1b40
Kaylee <kayleetoth@gmail.com>
2023-12-10 08:41:59 -0700

    First commit to repo

---
+++ src/NameTag.js
@@ -0,0 +1,23 @@
+// import the react library to write JSX
+import React from "react";
+
+/* write an arrow function called "NameTag" that returns JSX
+Please see practice "Pass Props to your Functional Component STEP 5 of 6"
+The JSX returned should consist of:
+  - a div element with className attribute of "name-tag" and three nested children elements:
+    - an h3 element with className attribute of "title" and the inner text of "HELLO"
+    - a p element with className attribute of "subtitle" and the inner text of "my name is"
+    - a h2 element with className attribute of "name" and the inner text of your name
+    - In JSX, it is alway important to close your tags <> 
+*/
+const NameTag = (props) => (
+  <div className="name-tag">
+    <h3 className="title">HELLO</h3>
+    <p className="subtitle">my name is</p>
+    <h2 className="name">{props.name}</h2>
+  </div>
+);
+/* export our functional component definition to be 
+imported then rendered within another module */
+
+export default NameTag;
---
+++ src/data.js
@@ -0,0 +1,10 @@
+export const names = [
+  { person: "Kaylee", id: "001" },
+  { person: "Shaun", id: "002" },
+  { person: "Lisa", id: "003" },
+  { person: "Troy", id: "004" },
+  { person: "Leanne", id: "005" },
+  { person: "Stu", id: "006" },
+  { person: "Susan", id: "007" },
+  { person: "Neil", id: "008" },
+];
